3d88d5849353fec7c981c95b47ad2f0c84fdadca
Seb-geospatial <sebastianbursey@gmail.com>
2025-01-06 16:16:42 -0500

    Added code to read NIR and red rasters into rasterio according to input path arguments

--- rasterioNDVI.py
+++ rasterioNDVI.py
@@ -2,5 +2,9 @@
 import rasterio
 
 # Define function to generate NDVI from input rasters
-def rasterioNDVI():
-    return
+def rasterioNDVI(NIR_path: str, red_path: str, output_path: str):
+    with rasterio.open(NIR_path) as src_NIR:
+        NIR_band = src_NIR.read(1)
+
+    with rasterio.open(red_path) as src_red:
+        red_band = src_red.read(1)
