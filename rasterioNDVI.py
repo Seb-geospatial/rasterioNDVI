5f151ce189f1f98cc800f085dd25926195ae147c
Seb-geospatial <sebastianbursey@gmail.com>
2025-01-06 22:20:15 -0500

    Changed functionality of script so that it is meant to be run using command line arguments as parameters for the rasterioNDVI() function whenever rasterioNDVI.py is run to simplify the process of using the script

--- rasterioNDVI.py
+++ rasterioNDVI.py
@@ -1,9 +1,15 @@
 # Import packages
 import rasterio
 import numpy as np
+import sys
+
+# Define command line arguments as parameters for rasterioNDVI() function
+nir_path_arg = str(sys.argv[1])
+red_path_arg = str(sys.argv[2])
+output_path_arg = str(sys.argv[3])
 
 # Define function to generate NDVI from input rasters
-def rasterioNDVI(nir_path: str, red_path: str, output_path: str):
+def rasterioNDVI(nir_path = nir_path_arg, red_path = red_path_arg, output_path = output_path_arg):
     # Open input rasters
     nir_raster = rasterio.open(nir_path)
     red_raster = rasterio.open(red_path)
@@ -25,3 +31,5 @@ def rasterioNDVI(nir_path: str, red_path: str, output_path: str):
     # Write NDVI to new GeoTIFF raster image
     with rasterio.open(output_path, 'w', **output_meta) as dst:
         dst.write(NDVI, 1)
+
+rasterioNDVI()
